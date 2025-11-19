a0537cec65196e7f673f0e940485654059d965a3
farhankdi <43706147+farieas@users.noreply.github.com>
2025-11-19 19:26:36 +0530

    Refactor idx-template.nix for directory and file handling

--- remix/idx-template.nix
+++ remix/idx-template.nix
@@ -8,15 +8,12 @@
     mkdir -p "$WS_NAME"
     npx create-remix@latest "$WS_NAME"
 
-    mkdir -p "$WS_NAME/.idx"
-    cp ${./dev.nix} "$WS_NAME/.idx/dev.nix"
+    mkdir -p "$WS_NAME/.idx/"
+    cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
 
-    chmod -R u+w "$WS_NAME"
-    mv "$WS_NAME" "$out"
-
-   chmod -R u+w "$out"
+    chmod -R u+w "$out"
     cd "$out"; npm install --package-lock-only --ignore-scripts
   '';
 }
