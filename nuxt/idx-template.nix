ec6d2584d30cd96b21c12329cdcd68b8dd9f0ac8
farhankdi <43706147+farieas@users.noreply.github.com>
2025-12-11 12:34:04 +0530

    Update idx-template.nix

--- nuxt/idx-template.nix
+++ nuxt/idx-template.nix
@@ -14,7 +14,7 @@
     # npx --yes nuxi@latest init nuxiapp --template ui --package-manager npm --no-questions --no-install
     bootstrap = ''
       npx -yes nuxi@latest init "$out"  \
-        --template ui
+        --template ui \
         --package-manager ${packageManager} \
         --no-install \
         --git-init no \
