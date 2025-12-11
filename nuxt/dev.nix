bc8d442f8866284697e6a034914bebd5c91853cf
farhankdi <43706147+farieas@users.noreply.github.com>
2025-12-11 13:05:28 +0530

    Update dev.nix

--- nuxt/dev.nix
+++ nuxt/dev.nix
@@ -5,7 +5,7 @@
   channel = "stable-25.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs
+    pkgs.nodejs_24
     PM_NIX_PACKAGE
   ];
   # Sets environment variables in the workspace
